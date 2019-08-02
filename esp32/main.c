134a6c2222df12f68190517b2cd1d1f1a68ac1ed
Kirishi Hackerspace <sw3n@mail.ru>
2019-08-03 01:18:10 +0300

    update

--- esp32/main.c
+++ esp32/main.c
@@ -68,7 +68,7 @@ void loop() {
         USE_SERIAL.print("[HTTP] begin...\n");
         // configure traged server and url
         //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-        http.begin("http://port47.dlinkddns.com/esp.php"); //HTTP
+        http.begin("http://YOURESERVER.com/esp.php"); //HTTP
 
         USE_SERIAL.print("[HTTP] GET...\n");
         // start connection and send HTTP header
